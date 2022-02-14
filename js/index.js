a1e5809c42ef2c0f01a72ee5693dbfc3e49684e9
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2022-02-13 21:40:33 -0400

    Complete web site

--- js/index.js
+++ js/index.js
@@ -6,11 +6,24 @@
         const feelings = document.getElementById("feelings");
         const feelingsBox = document.getElementById("feelings-box");
         let feelingsTrue = false;
+        //Song
+        const song = document.getElementById("song");
+        const songBox = document.getElementById("song-box");
+        let songTrue = false;
+        //Love
+        const love = document.getElementById("love");
+        const loveBox = document.getElementById("love-box");
+        let loveTrue = false;
 
+        //EVENT CLICK
         document.addEventListener("click", (e)=>{
+
             if(e.target === start){
                 if(!startTrue){
                     startBox.classList.remove("none");
+                    loveBox.classList.add("none");
+                    songBox.classList.add("none");
+                    feelingsBox.classList.add("none");
                     startTrue = true;
                 }else if(startTrue){
                     startBox.classList.add("none");
@@ -21,10 +34,39 @@
             if(e.target === feelings){
                 if(!feelingsTrue){
                     feelingsBox.classList.remove("none");
+                    loveBox.classList.add("none");
+                    songBox.classList.add("none");
+                    startBox.classList.add("none");
                     feelingsTrue = true;
                 }else if(feelingsTrue){
                     feelingsBox.classList.add("none");
                     feelingsTrue = false;
+                }
+            }
+
+            if(e.target === song){
+                if(!songTrue){
+                    songBox.classList.remove("none");
+                    loveBox.classList.add("none");
+                    feelingsBox.classList.add("none");
+                    startBox.classList.add("none");
+                    songTrue = true;
+                }else if(songTrue){
+                    songBox.classList.add("none");
+                    songTrue = false;
+                }
+            }
+
+            if(e.target === love){
+                if(!loveTrue){
+                    loveBox.classList.remove("none");
+                    songBox.classList.add("none");
+                    feelingsBox.classList.add("none");
+                    startBox.classList.add("none");
+                    loveTrue = true;
+                }else if(loveTrue){
+                    loveBox.classList.add("none");
+                    loveTrue = false;
                 }
             }
         });
